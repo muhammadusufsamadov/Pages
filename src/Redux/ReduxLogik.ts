ae24f1dfe9b1a98320ef405390ccb348daa834e1
muhammadusufsamadov <muhammadusufsamadov83@gmail.com>
2026-03-14 12:24:10 +0500

    chang

--- src/Redux/ReduxLogik.ts
+++ src/Redux/ReduxLogik.ts
@@ -1,23 +1,58 @@
 import { createSlice } from '@reduxjs/toolkit'
-import type { PayloadAction } from '@reduxjs/toolkit'
-
-export interface CounterState {
-  value: number
+export interface IData {
+    id: number,
+    name:string,
+    age:number,
+    status:boolean
 }
 
-const initialState: CounterState = {
-  value: 0,
+export interface CounterState {
+    data: IData[]
+}
+
+let initialState: CounterState = {
+    data: [
+        {
+            id:1, name:"Muhammad", age:12, status: false,
+        },
+        {
+            id:2, name:"Muhammad2", age:14, status:true
+        },
+        {
+            id:3, name:"Ali", age:21, status:true
+        },
+        {
+            id:4, name:"Ahmad", age:29, status:false
+        },
+        {
+            id:5, name:"Vali", age:16, status:true
+        }
+    ]
 }
 
 export const counterSlice = createSlice({
   name: 'counter',
   initialState,
   reducers: {
-    
-  },
+   deleteUser:(state, {payload}) => {
+    state.data = state.data.filter((user) => user.id !== payload)
+   },
+   changeStatus: (state, {payload}) => {
+    state.data = state.data.map((user) => user.id == payload ? {...user, status: !user.status } : user)
+   },
+   addNewUser: (state,{payload}) => {
+     state.data.push(payload)
+   },
+   editUser: (state,{payload}) => {
+    state.data = state.data.map((user) => 
+        user.id == payload.id ? payload : user
+    )
+   }
+}
 })
 
+
 // Action creators are generated for each case reducer function
-export const { } = counterSlice.actions
+export const {deleteUser, changeStatus, addNewUser, editUser} = counterSlice.actions
 
 export default counterSlice.reducer
--- src/store/redux.ts
+++ src/store/redux.ts
@@ -1,12 +1,13 @@
-import { configureStore, createSlice } from "@reduxjs/toolkit"
+import { configureStore } from "@reduxjs/toolkit";
+import createSlice from "../Redux/ReduxLogik";
 
 export const store = configureStore({
   reducer: {
-    count: createSlice
+    counter: createSlice,
   },
-})
+});
 
 // Infer the `RootState` and `AppDispatch` types from the store itself
-export type RootState = ReturnType<typeof store.getState>
+export type RootState = ReturnType<typeof store.getState>;
 // Inferred type: {posts: PostsState, comments: CommentsState, users: UsersState}
-export type AppDispatch = typeof store.dispatch
+export type AppDispatch = typeof store.dispatch;
