35dee39c35f4e57d8353fbc6e4933a2310aead88
muhammadusufsamadov <muhammadusufsamadov83@gmail.com>
2026-03-17 14:40:47 +0500

    all changes

--- src/App.tsx
+++ src/App.tsx
@@ -1,140 +1,127 @@
-import { useEffect, useState } from "react"
-import { useDispatch, useSelector } from "react-redux"
-import { AddNewUser, DeleteUser, GetUser } from "./redux/redux"
-import {useFormik} from "formik"
-import {Button, Input, Modal} from "antd"
-interface IUser{
-    name: string,
-    avatar: string,
-    age: string,
-    status: boolean,
-    location: string,
-    isLoading: boolean,
-    id: number
-}
+import React from 'react'
+import  { useEffect, useState } from 'react'
+import { useDispatch, useSelector } from 'react-redux'
+import { addData, deleteData, editData, getData, type IData } from './reducer/todo'
+import { Button, Input, Modal, Select } from 'antd'
+import { useFormik } from 'formik'
+import "./App.css"
 const App = () => {
-  let {data, isLoading} = useSelector((store) => store.counter)
+  const [idx,setIdx] = useState<string | null>(null)
+  const {data} = useSelector((state) => state.counter)
   const dispatch = useDispatch()
-  useEffect(() => {
-    dispatch(GetUser())
-  }, [])
-  console.log(data);
-
-  // addModal;
-  const [isModalOpenAdd, setIsModalOpenAdd] = useState(false);
-
-  const showModalAdd = () => {
-    setIsModalOpenAdd(true);
-  };
-
-  const handleOkAdd = () => {
-    setIsModalOpenAdd(false);
-  };
-
-  const handleCancelAdd = () => {
-    setIsModalOpenAdd(false);
-  };
-  // editModal;
-  const [isModalOpenEdit, setIsModalOpenEdit] = useState(false);
-
-  const showModalEdit = () => {
-    setIsModalOpenEdit(true);
-  };
-
-  const handleOkEdit = () => {
-    setIsModalOpenEdit(false);
-  };
-
-  const handleCancelEdit = () => {
-    setIsModalOpenEdit(false);
-  };
+  useEffect(() =>
+    {
+     dispatch( getData())
+    },[])
+  const {values,handleChange,handleSubmit,setFieldValue,resetForm} = useFormik(
+    {
+      initialValues:
+      {
+        name:"",
+        avatar:"",
+        age:0,
+        location:"",
+        status:false
+      },
+      onSubmit:(value) =>
+      {
+        if(idx == null){
+          dispatch(
+            addData(
+            {
+              id: Date.now(),...value
+            }))
+            resetForm()
+            handleCancel()
+          }
+          else
+            {
+             dispatch( editData(
+                {
+                 id: idx, ...value
+                }))
+                resetForm()
+                handleCancelE()
+            }
+          }
+    })
   
-  let {handleChange, handleSubmit, setFieldValue, resetForm, values} = useFormik({
-    initialValues:{
-      name:"",
-      avatar:"",
-      age:"",
-      status:false,
-      location:""
-    },
-    onSubmit:(values)=> {
-      if(idx) {
-       dispatch(<EditUser></EditUser>({id: idx, ...values}))
-        handleCancelAdd()
-        resetForm()
-      }
-      else{
-        dispatch(AddNewUser(values))
-        handleCancelAdd()
-        resetForm()
-      }
+    function handleUser(user:IData)
+    {
+      setIdx(user.id)
+      setFieldValue("name",user.name)
+      setFieldValue("age",user.age)
+      setFieldValue("location",user.location)
+      setFieldValue("status",user.status)
     }
-  })
-  let [idx, setIdx] = useState(null)
-  let handleEdit = (user:IUser) => {
-    setIdx(user.id)
-    setFieldValue("name", user.name)
-    setFieldValue("avatar", user.avatar)
-    setFieldValue("age", user.age)
-    setFieldValue("location", user.location)
-    setFieldValue("status", user.status)
-  }
-  if(isLoading) {
- return <div>Loading........</div>
-  }
+
+  const [isModalOpen, setIsModalOpen] = useState(false);
+  const [isModalOpenE, setIsModalOpenE] = useState(false);
+
+  const showModal = () => {
+    setIsModalOpen(true);
+  };
+
+  const handleCancel = () => {
+    setIsModalOpen(false);
+  };
+
+  
+  const showModalE = () => {
+    setIsModalOpenE(true);
+  };
+
+  const handleCancelE = () => {
+    setIsModalOpenE(false);
+  };
+
   return (
-  <>
-      <Button type="primary" onClick={showModalAdd}>+ Add New</Button>
-    <div className="grid grid-cols-3 w-[69%] gap-[20px] m-auto mt-[40px]">
-      {data.map((user:IUser) => {
-        return <div className="shadow-lg w-[280px] h-[180px] flex flex-col items-center justify-center gap-[20px] rounded-3xl">
-          <h1>{user.name}</h1>
-          <div className="flex gap-[10px]">
-          <Button type="primary" onClick={() => {showModalEdit(), handleEdit(user)}}>Edit</Button>
-          <Button danger onClick={() => dispatch(DeleteUser(user.id))}>Delete</Button>
-          </div>
+    <>
+    <div>
+      <Button type='primary' onClick={showModal}>Add New +</Button>
+    </div>
+    <div className='grid grid-cols-3 w-[76%] gap-[30px] m-auto'>
+
+      {data.map((user:IData) => {
+        return <div className='shadow-lg flex flex-col items-center justify-center w-[300px] h-[230px] rounded-3xl'>
+          <h1 className='flex text-center flex m-auto'>{user.name}</h1>
+          <h1 className='flex text-center flex m-auto'>{user.age}</h1>
+          <h1 className='flex text-center flex m-auto'><span className={user.status ? "Active" : "Inactive"}>{user.status ? "Active" : "Inactive"}</span></h1>
+         <div className='flex gap-[20px] pb-[10px]'>
+       <Button onClick={() => dispatch(deleteData(user.id))}>delete</Button>
+       <Button onClick={() => {showModal() , handleUser(user)}}>Edit</Button>
+         </div>
         </div>
       })}
+  
+  <Modal
+        title="Basic Modal"
+        closable={{ 'aria-label': 'Custom Close Button' }}
+        open={isModalOpenE}
+        onCancel={handleCancelE}
+        >
+        <form onSubmit={handleSubmit}>
+          <Input name='name' value={values.name} onChange={handleChange}/>
+          <Input name='age' value={values.age} onChange={handleChange}/>
+          <Input name='location' value={values.location} onChange={handleChange}/>
+          <button type='submit'>Save</button>
+        </form>
+      </Modal>
+  <Modal
+        title="Basic Modal"
+        closable={{ 'aria-label': 'Custom Close Button' }}
+        open={isModalOpen}
+        onCancel={handleCancel}
+        >
+        <form onSubmit={handleSubmit}>
+          <Input name='name' value={values.name} onChange={handleChange}/>
+          <Input name='age' value={values.age} onChange={handleChange}/>
+          <Input name='location' value={values.location} onChange={handleChange}/>
+          <button type='submit'>Save</button>
+        </form>
+      </Modal>
     </div>
-    <Modal
-        title="Basic Modal"
-        closable={{ 'aria-label': 'Custom Close Button' }}
-        open={isModalOpenAdd}
-        onOk={handleOkAdd}
-        onCancel={handleCancelAdd}
-      >
-       <form onSubmit={handleSubmit}>
-        <Input name="name" value={values.name} onChange={handleChange} placeholder="Name"/>
-        <Input name="avatar" value={values.avatar} onChange={handleChange} placeholder="Avatar"/>
-        <Input name="age" value={values.age} onChange={handleChange} placeholder="Age"/>
-        <Input name="location" value={values.location} onChange={handleChange} placeholder="Location"/>
-      <select name="status" onChange={handleChange} value={values.status.toString()} id="">
-        <option value="true">Active</option>
-        <option value="false">Inactive</option>
-      </select>
-      <button type="submit">Save</button>
-       </form>
-      </Modal>
-    <Modal
-        title="Basic Modal"
-        closable={{ 'aria-label': 'Custom Close Button' }}
-        open={isModalOpenEdit}
-        onOk={handleOkEdit}
-        onCancel={handleCancelEdit}
-      >
-       <form onSubmit={handleSubmit}>
-        <Input name="name" value={values.name} onChange={handleChange} placeholder="Name"/>
-        <Input name="avatar" value={values.avatar} onChange={handleChange} placeholder="Avatar"/>
-        <Input name="age" value={values.age} onChange={handleChange} placeholder="Age"/>
-        <Input name="location" value={values.location} onChange={handleChange} placeholder="Location"/>
-      <select name="status" onChange={handleChange} value={values.status.toString()} id="">
-        <option value="true">Active</option>
-        <option value="false">Inactive</option>
-      </select>
-      <button type="submit">Save</button>
-       </form>
-      </Modal>
-      </>
+        </>
   )
 }
 
--- src/store/store.ts
+++ src/store/store.ts
@@ -1,13 +1,13 @@
-import { configureStore } from "@reduxjs/toolkit";
-import counterSlice from "../redux/redux";
+import { configureStore } from '@reduxjs/toolkit'
+import counterReducer from '../reducer/todo'
 
 export const store = configureStore({
   reducer: {
-    counter: counterSlice,
+    counter: counterReducer,
   },
-});
+})
 
 // Infer the `RootState` and `AppDispatch` types from the store itself
-export type RootState = ReturnType<typeof store.getState>;
+export type RootState = ReturnType<typeof store.getState>
 // Inferred type: {posts: PostsState, comments: CommentsState, users: UsersState}
-export type AppDispatch = typeof store.dispatch;
+export type AppDispatch = typeof store.dispatch
