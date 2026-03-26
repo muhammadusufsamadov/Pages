52d88690d4ec95e92f946daecd6ba1c003624083
muhammadusufsamadov <muhammadusufsamadov83@gmail.com>
2026-03-26 15:38:55 +0500

    feat: initial commit

---
+++ src/Backend.ts
@@ -0,0 +1,8 @@
+export const salesData = [
+  { month: 'Jan', sales: 4000 },
+  { month: 'Feb', sales: 3000 },
+  { month: 'Mar', sales: 5000 },
+  { month: 'Apr', sales: 4780 },
+  { month: 'May', sales: 5890 },
+  { month: 'Jun', sales: 4390 },
+];
